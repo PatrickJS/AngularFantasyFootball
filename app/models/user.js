ff40112800cea712f92c402ab2317247115f483a
gdi2290 <github@gdi2290.com>
2013-09-01 16:18:54 -0700

    fix format

--- app/models/user.js
+++ app/models/user.js
@@ -3,10 +3,10 @@
  * Module dependencies.
  */
 
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
-  , crypto = require('crypto')
-  , _ = require('underscore')
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    crypto = require('crypto'),
+    _ = require('underscore');
 
 /**
  * User Schema
@@ -19,7 +19,7 @@ var UserSchema = new Schema({
   provider: String,
   hashed_password: String,
   salt: String
-})
+});
 
 /**
  * Virtuals
@@ -28,37 +28,39 @@ var UserSchema = new Schema({
 UserSchema
   .virtual('password')
   .set(function(password) {
-    this._password = password
-    this.salt = this.makeSalt()
-    this.hashed_password = this.encryptPassword(password)
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
   })
-  .get(function() { return this._password })
+  .get(function() {
+    return this._password;
+  });
 
 /**
  * Validations
  */
 
-var validatePresenceOf = function (value) {
-  return value && value.length
-}
+var validatePresenceOf = function(value) {
+  return value && value.length;
+};
 
 // the below 4 validations only apply if you are signing up traditionally
 
 UserSchema.path('name').validate(function (name) {
-  return name.length
-}, 'Name cannot be blank')
+  return name.length;
+}, 'Name cannot be blank');
 
 UserSchema.path('email').validate(function (email) {
-  return email.length
-}, 'Email cannot be blank')
+  return email.length;
+}, 'Email cannot be blank');
 
 UserSchema.path('username').validate(function (username) {
-  return username.length
-}, 'Username cannot be blank')
+  return username.length;
+}, 'Username cannot be blank');
 
 UserSchema.path('hashed_password').validate(function (hashed_password) {
-  return hashed_password.length
-}, 'Password cannot be blank')
+  return hashed_password.length;
+}, 'Password cannot be blank');
 
 
 /**
@@ -66,13 +68,14 @@ UserSchema.path('hashed_password').validate(function (hashed_password) {
  */
 
 UserSchema.pre('save', function(next) {
-  if (!this.isNew) return next()
+  if (!this.isNew) return next();
 
-  if (!validatePresenceOf(this.password))
-    next(new Error('Invalid password'))
-  else
-    next()
-})
+  if (!validatePresenceOf(this.password)) {
+    next(new Error('Invalid password'));
+  } else {
+    next();
+  }
+});
 
 /**
  * Methods
@@ -89,7 +92,7 @@ UserSchema.methods = {
    */
 
   authenticate: function(plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password
+    return this.encryptPassword(plainText) === this.hashed_password;
   },
 
   /**
@@ -100,7 +103,7 @@ UserSchema.methods = {
    */
 
   makeSalt: function() {
-    return Math.round((new Date().valueOf() * Math.random())) + ''
+    return Math.round((new Date().valueOf() * Math.random())) + '';
   },
 
   /**
@@ -112,9 +115,9 @@ UserSchema.methods = {
    */
 
   encryptPassword: function(password) {
-    if (!password) return ''
-    return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+    if (!password) return '';
+    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
   }
-}
+};
 
-mongoose.model('User', UserSchema)
+mongoose.model('User', UserSchema);
