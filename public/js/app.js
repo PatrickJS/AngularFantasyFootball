4eccd89385c18d0463366c6c48277b74234a7dae
gdi2290 <github@gdi2290.com>
2013-09-02 16:07:36 -0700

    cleaner format

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,20 @@
-window.app = angular.module('ngFantasyFootball', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', /*'restangular',*/ 'ngff.controllers', 'ngff.directives', 'ngff.services']);
+window.app =
+  angular.module('ngFantasyFootball', [
+    'ngCookies',
+    'ngResource',
+    'ui.bootstrap',
+    'ngRoute',
+    /*'restangular',*/
+    'ngff.controllers',
+    'ngff.directives',
+    'ngff.services'
+  ]);
 
 // bundling dependencies
-window.angular.module('ngff.controllers', ['ngff.controllers.header','ngff.controllers.index']);
-window.angular.module('ngff.services', ['ngff.services.global']);
+window.angular.module('ngff.controllers', [
+  'ngff.controllers.header',
+  'ngff.controllers.index'
+]);
+window.angular.module('ngff.services', [
+  'ngff.services.global'
+]);
