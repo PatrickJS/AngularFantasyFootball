9d683f506683dff59331a861e07ad5f0096ebf33
gdi2290 <github@gdi2290.com>
2013-09-02 16:55:07 -0700

    create NFL controller

---
+++ public/js/controllers/nlf.js
@@ -0,0 +1,5 @@
+window.angular.module('ngff.controllers.nlf', [])
+  .controller('NFLController', [ '$scope', '$routeParams', 'Global', 'NFL',
+    function($scope, $routeParams, Global, NFL) {
+    }
+  ]);
