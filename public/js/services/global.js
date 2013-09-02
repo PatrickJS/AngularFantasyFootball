c17d52f9a696868d0026a033f6945d3428045dae
gdi2290 <github@gdi2290.com>
2013-09-02 16:02:42 -0700

    inject the Global user

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -1,4 +1,6 @@
 window.angular.module('ngff.services.global', [])
-  .factory('Global', function() {
+  .factory('Global', [ '$window', function($window) {
+    var current_user = $window.user;
 
-  });
+    return current_user;
+  }]);
