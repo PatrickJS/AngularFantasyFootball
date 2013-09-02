3966ce4e7b2625fc03387221c7bfa7c52b0edf90
gdi2290 <github@gdi2290.com>
2013-09-02 16:55:58 -0700

    add global, nfl teams, and nfl team

--- public/js/controllers/nlf.js
+++ public/js/controllers/nlf.js
@@ -1,5 +1,9 @@
 window.angular.module('ngff.controllers.nlf', [])
   .controller('NFLController', [ '$scope', '$routeParams', 'Global', 'NFL',
     function($scope, $routeParams, Global, NFL) {
+      $scope.global = Global;
+
+      $scope.nlfteams = NFL.teams;
+      $scope.nflteam  =  NFL.teams[$routeParams['nflTeamId']];
     }
   ]);
