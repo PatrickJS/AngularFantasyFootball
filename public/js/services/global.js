6c0c7287c4b43151478d073a8c23bb5118b3e555
gdi2290 <github@gdi2290.com>
2013-09-02 16:04:20 -0700

    encapsulate current_user

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -2,5 +2,12 @@ window.angular.module('ngff.services.global', [])
   .factory('Global', [ '$window', function($window) {
     var current_user = $window.user;
 
-    return current_user;
+    return {
+      currentUser: function() {
+        return current_user;
+      },
+      isSignedIn: function() {
+        return !!current_user;
+      }
+    };
   }]);
