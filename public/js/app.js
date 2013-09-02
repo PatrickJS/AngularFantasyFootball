7044ee5d191dd45605ca1592dd141ba34af0f0e2
gdi2290 <github@gdi2290.com>
2013-09-02 15:57:17 -0700

    add Restangular

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-window.app = angular.module('ngFantasyFootball', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'ngff.controllers', 'ngff.directives', 'ngff.services']);
+window.app = angular.module('ngFantasyFootball', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', /*'restangular',*/ 'ngff.controllers', 'ngff.directives', 'ngff.services']);
 
 // bundling dependencies
 window.angular.module('ngff.controllers', ['ngff.controllers.header','ngff.controllers.index']);
